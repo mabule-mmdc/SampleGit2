11e72893744c5109de080f39fa064bcdddcc6461
Micah Andrew Bule <micah.bule@gmail.com>
2023-05-08 20:53:43 +0800

    Remove incompatible classes for now

--- src/main/java/com/mmdc/samplegit2/SampleGit2.java
+++ src/main/java/com/mmdc/samplegit2/SampleGit2.java
@@ -14,17 +14,6 @@ import java.awt.*;
 public class SampleGit2 {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Hello world");
-
         System.out.println("Hello Mapua Malayan Digital College!");
-
-        SampleGui sampleGui = new SampleGui();
-
-        frame.setContentPane(sampleGui.mainPanel);
-
-        frame.setPreferredSize(new Dimension(800, 500));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setFocusable(true);
-        frame.setVisible(true);
     }
 }
