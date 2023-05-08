ffe9f4bbc10d00a581e8566948f4413efa5d0209
Micah Andrew Bule <micah.bule@gmail.com>
2023-05-08 20:11:39 +0800

    initial commit

---
+++ src/main/java/com/mmdc/samplegit2/SampleGit2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mmdc.samplegit2;
+
+/**
+ *
+ * @author micahbule
+ */
+public class SampleGit2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Mico Bule!");
+    }
+}
