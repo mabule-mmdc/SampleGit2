53c6e3eb3c69ac07779ddc8b3d28b271c3660bee
Micah Andrew Bule <micah.bule@gmail.com>
2023-05-08 21:07:42 +0800

    Added login class

---
+++ src/main/java/com/mmdc/samplegit2/Login.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mmdc.samplegit2;
+
+/**
+ *
+ * @author micahbule
+ */
+public class Login {
+    private String username;
+    private String password;
+}
